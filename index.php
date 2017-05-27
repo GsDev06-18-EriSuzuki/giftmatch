f0bb493be6f5a275f238226b051cbe99e7678cb3
GsDev06-18-EriSuzuki <ahuredasu.suzuki@gmail.com>
2017-05-27 17:01:51 +0900

    no message

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>GiftMatch</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <script src="js/jquery-2.1.0.min.js"></script>
+</head>
+<body>
+
+  <header>
+    <div class="container">
+
+<!--ロゴー-->
+      <div class="header-left">
+        <img class="logo" src="image/logo-orange.png">
+      </div>
+
+<!--ナビゲーションバー-->
+      <div class="header-right">
+        <nav class="global_Nav">
+          <ul class="nav_wrap clearfix">
+          <li class="nav_item"><a href="#feature">特徴</a></li>
+          <li class="nav_item"><a href="#concept">コンセプト</a></li>
+          <li class="nav_item"><a href="#howto">使い方</a></li>
+          <li class="nav_item"><a href="#case">事例</a></li>
+          <li class="nav_item"><a href="#">ログイン</a></li>
+          <li class="nav_item"><a href="#signup">会員登録</a></li>
+        </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+
+<!--トップー-->
+  <div class="top-wrapper">
+    <div class="container">
+      <h1>偏りを活かして</h1>
+      <h1>自分らしい働き方をしよう</h1>
+      <p>GiftMatchは、グレーゾーンや発達障害で、強みを活かしてハイスキルな仕事をしたい人のための<br>
+      クラウドソーシング＆ジョブマッチングサイトです。<br>
+      得意を活かして実績を積み、自分らしい働き方をしたい方の仕事探しを応援します。</p>
+      <div class="btn-wrapper">
+        <a href="#" class="btn signup">新規登録はこちら</a>
+      </div>
+    </div>
+  </div>
+
+
+
+<!--特徴ー-->
+  <div id="feature" class="feature-wrapper">
+    <div class="container">
+     <h2 >SERVICE</h2>
+      <div class="features">
+        <div class="feature">
+            <img src="image/roope.png">
+            <h3>仕事を探す</h3>
+          <p>ウェブページの作成に使用される言語です。HTMLとCSSを組み合わせることで、静的なページを作り上げることができます。</p>
+        </div>
+        <div class="feature">
+            <img src="image/score.png">
+            <h3>実績をつむ</h3>
+          <p>ウェブページの作成に使用される言語です。HTMLとCSSを組み合わせることで、静的なページを作り上げることができます。</p>
+        </div>
+        <div class="feature">
+            <img src="image/add.png">
+            <h3>企業オファー</h3>
+          <p>ウェブページの作成に使用される言語です。HTMLとCSSを組み合わせることで、静的なページを作り上げることができます。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<!--コンセプトー-->
+  <div id="concept" class="concept-wrapper">
+    <div class="container">
+        <h2>CONCEPT</h2>
+        <h3>世界を震わすチーズを創ろう。</h3>
+        <p>
+            今、世界中の人たちが足りないと感じている、栄養素があります。<br />
+            その栄養は『カルシウム』と『マグネシウム』<br />
+            小さい子供の成長に欠かせないカルシウム<br />
+            イライラをなくすには欠かせないカルシウム<br />
+            今まで食べたことのないチーズから取れるカルシウム<br />
+            そんな悩みを抱えているあなたこそ、<br />
+            プロレベルのチーズ作りスキルを持つべきだと思うのです。<br />
+        </p>
+        <p>
+            できるだけ多くの若い人に本格的なチーズ作りのスキルを学ぶ機会を創りたい。<br />
+            そして願わくば、この場所から世界中の多くの人がおいしいと言えるような新感覚のチーズが生まれてほしい。
+        </p>
+    </div>
+  </div>
+
+
+<!--使い方ー-->
+  <div id="howto" class="howto-wrapper">
+    <div class="container">
+        <h2>HOW TO USE</h2>
+      <div class="howtos">
+        <div class="howto">
+            <img src="image/jobsearch.png">
+            <a href="#" class="btn signup">登録して仕事を受ける</a>
+        </div>
+        <div class="howto">
+            <img src="image/joborder.png">
+            <a href="#" class="btn green">登録して仕事を依頼する</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<!--事例ー-->
+  <div id="case" class="case-wrapper">
+    <div class="container">
+        <h2>CASE</h2>
+      <div class="cases">
+        <div class="case">
+            <img src="image/pazuru.jpg">
+            <h3>タイトル1</h3>
+          <p class="case-text">テキストテキストテキスト</p>
+        </div>
+      </div>
+      <div class="cases">
+        <div class="case">
+            <img src="image/pazuru.jpg">
+            <h3>タイトル2</h3>
+          <p class="case-text">テキストテキストテキスト</p>
+        </div>
+      </div>
+      <div class="cases">
+        <div class="case">
+            <img src="image/pazuru.jpg">
+            <h3>タイトル3</h3>
+          <p class="case-text">テキストテキストテキスト</p>
+        </div>
+      </div>
+      <a href="#">and more...</a>
+    </div>
+  </div>
+
+
+<!--メッセージー-->
+  <div class="message-wrapper">
+    <div class="container">
+        <h2>さぁ、あなたも偏りを活かして自分らしく働きませんか?</h2>
+      </div>
+      <span class="btn message">会員登録してはじめる</span>
+    </div>
+  </div>
+
+
+
+
+<!--フッターー-->
+  <footer>
+    <div class="container">
+      <img src="image/logo-orange.png">
+      <p>copyright (c) GiftMatch all rights reserved.</p>
+    </div>
+  </footer>
+
+
+</body>
+</html>
